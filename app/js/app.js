a8d3540886ec1e75c3f3095e5771e503ac5c6879
andrey-maslov <andrei.maslau@gmail.com>
2020-11-19 11:04:25 +0300

    mobile safary bugs fixed

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-	// Custom JS
+    const langToggle = document.getElementById('langToggle')
+    const navToggle = document.getElementById('navToggle')
+
+    openPopup(langToggle)
+    openPopup(navToggle)
+
+    function openPopup(toggleElement) {
+
+        const wrapper = toggleElement.parentElement
+
+        toggleElement.addEventListener("click", function (e) {
+            e.stopPropagation()
+            wrapper.classList.toggle('opened')
+        })
+
+        document.body.addEventListener("click", function (e) {
+            if (wrapper.classList.contains('opened') && e.target !== toggleElement) {
+                wrapper.classList.remove('opened')
+            }
+        })
+    }
 
 });
